bba2b70d90049e92e885b011f534586c95b09ad6
Anand Mahurakar <85636775+anandmahurakar@users.noreply.github.com>
2022-09-22 07:37:10 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Hello CloudZenix.!     Good Morning everyone !";
+		return "Hello CloudZenix.!  Good Morning everyone..!";
 	}
 
 //	echo "# github-actions-example" >> README.md
